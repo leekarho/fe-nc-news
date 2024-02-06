783eaecd7a069a8605048fcd694ff78786f36d03
karho.lee <k-h_lee@hotmail.co.uk>
2024-02-06 10:02:51 +0000

    6- can get comments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-import "../styles/Header.css";
+import styles from "../styles/Header.module.css";
 
 export default function Header() {
   return (
     <>
-      <div className="header-class">
+      <div className={styles.header}>
         <h1>NC NEWS</h1>
         <p>Topic</p>
         <Link to="/login">
