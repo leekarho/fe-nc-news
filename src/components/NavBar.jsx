5d5ad3f801100a4ab1cecae368c31815505620de
karho.lee <k-h_lee@hotmail.co.uk>
2024-02-09 16:07:05 +0000

    add signin functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,19 +6,26 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   const { loggedInUser } = useContext(UserContext);
+
+  const isEmpty = Object.keys(loggedInUser).length === 0;
+
   return (
     <>
       <div className={styles.topicUser}>
         <TopicDropdown />
         <Link to="login">
-          <p>
-            <img
-              className={styles.image}
-              src={loggedInUser.avatar_url}
-              alt="user avatar"
-            />
-            {loggedInUser.username}
-          </p>
+          {isEmpty ? (
+            <p className={styles.signIn}>Sign-in</p>
+          ) : (
+            <p>
+              <img
+                className={styles.image}
+                src={loggedInUser.avatar_url}
+                alt="user avatar"
+              />
+              {loggedInUser.username}
+            </p>
+          )}
         </Link>
       </div>
     </>
