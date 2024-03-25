67794d4c377da9b7742dc18f4957d2fcec47a5ea
Kar-ho Lee <k-h_lee@hotmail.co.uk>
2024-03-25 14:37:37 +0000

    styling changes

--- src/components/ArticlesManager.jsx
+++ src/components/ArticlesManager.jsx
@@ -63,7 +63,6 @@ export default function ArticlesManager() {
           page={page}
           setPage={setPage}
         />
-        
       </section>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
 import { Link } from "react-router-dom";
-import DiamondOutlinedIcon from "@mui/icons-material/DiamondOutlined";
 import styles from "../styles/Header.module.css";
 
 export default function Header() {
   return (
     <>
       <div className={styles.logo}>
-        <DiamondOutlinedIcon />
         <Link to="/">
           <h1 className={styles.heading}>NC NEWS</h1>
         </Link>
